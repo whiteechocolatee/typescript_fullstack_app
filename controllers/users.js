1db2e11eca6e3d8f6bcb73296fabc622f9972b75
Andrey Chorniy <creatuseandr@icloud.com>
2023-04-29 14:46:33 +0300

    create login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,34 @@
+const { prisma } = require('../prisma/prisma-client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const login = async (req, res) => {
-  res.send('login');
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    return res.status(400).json({
+      message: 'Please, fill out required fields!',
+    });
+  }
+
+  const user = await prisma.user.findFirst({
+    where: { email },
+  });
+
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
+
+  if (user && isPasswordCorrect) {
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ message: 'Incorrect email or password' });
+  }
 };
 const register = async (req, res) => {
   res.send('login');
