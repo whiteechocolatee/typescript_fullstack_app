fa154c0ee01d561941f528a97dad03cd3c2579a5
Andrey Chorniy <creatuseandr@icloud.com>
2023-04-29 17:49:41 +0300

    add employees route to main app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 require('dotenv').config();
 
 const usersRouter = require('./routes/users');
+const employeesRouter = require('./routes/employees');
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/users', usersRouter);
+app.use('/api/employees', employeesRouter);
 
 module.exports = app;
