b8dd2475cd59d64343d24b3225a4a1fed9a1c5c4
Andrey Chorniy <creatuseandr@icloud.com>
2023-04-30 17:54:18 +0300

    create login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import { Card, Form, Row, Space, Typography } from 'antd';
 import { Layout } from '../../components/layout';
+import { CustomInput } from '../../components/custom-input';
+import { PasswordInput } from '../../components/password-input';
+import { CustomButton } from '../../components/custom-btn';
+import { Link } from 'react-router-dom';
+import { Paths } from '../../paths';
 
 export const Login = () => {
   return (
     <Layout>
-      <div>Login</div>
+      <Row align='middle' justify='center'>
+        <Card title='Log In' style={{ width: '30rem' }}>
+          <Form onFinish={() => null}>
+            <CustomInput
+              type='email'
+              name='email'
+              placeholder='E-mail'
+            />
+            <PasswordInput
+              name='password'
+              placeholder='Password'
+            />
+            <CustomButton type='primary' htmlType='submit'>
+              Log In
+            </CustomButton>
+          </Form>
+          <Space direction='vertical' size='large'>
+            <Typography>
+              New to Employee?
+              <Link to={Paths.register}>Sign Up</Link>
+            </Typography>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   );
 };
